225828c171e7f9d0f1516b8500b90eb4498e3414
Dennis Naujokat - Tower <prinzdarknis@googlemail.de>
2020-11-28 20:39:10 +0200

    prepair Angular

--- myProject/angular-scr/src/app/app-routing.module.ts
+++ myProject/angular-scr/src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from './guards/auth.guard';
-
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BooksComponent } from './components/books/books.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BookEditComponent } from './components/book-edit/book-edit.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [AuthGuard],
-  },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'books/:id', component: BookDetailsComponent },
+  { path: 'books/:id/edit', component: BookEditComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
---
+++ myProject/angular-scr/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BooksComponent } from './components/books/books.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BookEditComponent } from './components/book-edit/book-edit.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    BooksComponent,
+    BookDetailsComponent,
+    BookEditComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FlashMessagesModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
